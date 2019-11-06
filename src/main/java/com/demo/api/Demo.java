47ede724ad6617ea7f491731d833fe192911efee
yangyingan <1311954039@qq.com>
2019-11-06 11:50:40 +0800

    demo6

--- src/main/java/com/demo/api/Demo.java
+++ src/main/java/com/demo/api/Demo.java
@@ -5,4 +5,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("main");
     }
+
+    public static void test1(){
+        System.out.println("yya/demo6分支增加的内容");
+    }
 }
