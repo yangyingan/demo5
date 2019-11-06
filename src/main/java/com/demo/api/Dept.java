3676a1eabb95ee1b181c3dc3fe1d92608ea9b1d6
yangyingan <1311954039@qq.com>
2019-11-06 10:58:05 +0800

    部门

---
+++ src/main/java/com/demo/api/Dept.java
@@ -0,0 +1,13 @@
+package com.demo.api;
+
+public class Dept {
+    private String deptName;
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
