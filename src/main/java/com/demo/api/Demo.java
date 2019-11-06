5eba4e4e61bc8180e9d9a585aea464be2855536f
yangyingan <1311954039@qq.com>
2019-11-06 18:55:57 +0800

    增加test2方法

--- src/main/java/com/demo/api/Demo.java
+++ src/main/java/com/demo/api/Demo.java
@@ -5,4 +5,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("main");
     }
+
+    public static void test2(){
+        System.out.println("yya/demo5分支增加的内容");
+    }
 }
