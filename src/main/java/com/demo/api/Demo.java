25cc50b37d12b5f4cedcb8b562d13422577788c3
yangyingan <1311954039@qq.com>
2019-11-07 14:19:32 +0800

    增加test3方法

--- src/main/java/com/demo/api/Demo.java
+++ src/main/java/com/demo/api/Demo.java
@@ -1,8 +1,9 @@
 package com.demo.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Demo {
     public static void main(String[] args) {
         System.out.println("main");
